ce5564ec6b52e04719cc4e0a746440ba8b6115fd
ichi-t <taisuke_ichimura@icloud.com>
2019-06-11 01:49:54 +0900

    Add ichi-t keymap for claw44_ble

--- keyboards/claw44_ble/rules.mk
+++ keyboards/claw44_ble/rules.mk
@@ -10,7 +10,9 @@ COMMAND_ENABLE = yes    # Commands for debug and configuration
 SLEEP_LED_ENABLE = yes  # Breathing sleep LED during USB suspend
 NKRO_ENABLE = no	    # USB Nkey Rollover
 CUSTOM_MATRIX = yes # This flag should be on for nrf52
-
+BLE_NUS_MAX_INTERVAL = 20
+BLE_NUS_MAX_INTERVAL = 20
+BLE_HID_SLAVE_LATENCY = 2
 ## chip/board settings
 MCU_FAMILY = NRF52
 # linker script to use
